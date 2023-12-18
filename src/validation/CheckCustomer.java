1286887b73841f46ab0c5744bbedb6892ada1b69
keisha74 <keishaghazali22@gmail.com>
2023-12-18 14:23:28 +0700

    membuat check customer

--- src/validation/CheckCustomer.java
+++ src/validation/CheckCustomer.java
@@ -1,5 +1,28 @@
 package validation;
 
+import java.util.Scanner;
+
 public class CheckCustomer implements Validate{
-    public void validateId();
+    public boolean validateId(String id, String cabang){
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+
+        do{
+            // printCustomerList(--);
+            System.out.println("Input 0 to go back");
+            System.out.print("Input ID: ");
+            try {
+                input = sc.nextLine();
+            } catch (Exception e) {
+                System.out.println(e);
+                sc.nextLine();
+            }
+            if(input.compareTo("0") == 0) return true;
+        }while(input.length() != 10);
+        // if(cabang.compareTo(--) == 0) return false;
+        // else{
+        //     return true;
+        // }
+        return true;
+    }
 }
