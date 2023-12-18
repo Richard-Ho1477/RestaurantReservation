024041badc76eadf6c7bbd3533f7848ec8cd8695
keisha74 <keishaghazali22@gmail.com>
2023-12-18 13:37:07 +0700

    mengubah main

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # RestaurantReservation
+
+try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// Start connection
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/grocerystore","root","");
+			// ambil data
+			getData(con,"select * from items");
+			//insert data
+//			insertData(con, "insert into items (name,price,id) values (?,?,?)", "teh pucuk", 6000, 4);
+			// Update data
+//			updateData(con, "update items set name = ?, price = ? where id = ?", "Sabun mandi", 7000, 1);
+			//Delete Data
+//			deleteData(con, "delete from items where name = ?", "teh pucuk");
+			//close connection to db
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+        
+public static void getData(Connection con, String query) {
+		try {
+			// Select query
+			Statement stmt = con.createStatement();
+			// Query yang kita mau panggil
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				System.out.println("Name: " + rs.getString("name"));
+				System.out.println("Price: " + rs.getInt("price"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void insertData(Connection con, String query, String name, int price, int id) {
+		try {
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.setString(1, name);
+			pst.setInt(2, price);
+			pst.setInt(3, id);
+			int check = pst.executeUpdate(); // value klo 0 berarti ga ada yang nambah
+			if(check != 0) System.out.println("Data inserted");
+			else System.out.println("Data not inserted");
+			getData(con,"select * from items");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void updateData(Connection con, String query, String name, int price, int id) {
+		try {
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.setString(1, name);
+			pst.setInt(2, price);
+			pst.setInt(3, id);
+			int check = pst.executeUpdate(); // value klo 0 berarti ga ada yang ke update
+			if(check != 0) System.out.println("Data updated");
+			else System.out.println("Data not udpated");
+			getData(con,"select * from items");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void deleteData(Connection con, String query, String name) {
+		try {
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.setString(1, name);
+			int check = pst.executeUpdate(); // value klo 0 berarti ga ada yang ke delete
+			if(check != 0) System.out.println("Data deleted");
+			else System.out.println("Data not deleted");
+			getData(con,"select * from items");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,86 +1,66 @@
 package main;
 
 import java.sql.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			// Start connection
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/grocerystore","root","");
-			// ambil data
-			getData(con,"select * from items");
-			//insert data
-//			insertData(con, "insert into items (name,price,id) values (?,?,?)", "teh pucuk", 6000, 4);
-			// Update data
-//			updateData(con, "update items set name = ?, price = ? where id = ?", "Sabun mandi", 7000, 1);
-			//Delete Data
-//			deleteData(con, "delete from items where name = ?", "teh pucuk");
-			//close connection to db
-			con.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
-	
-	public static void getData(Connection con, String query) {
-		try {
-			// Select query
-			Statement stmt = con.createStatement();
-			// Query yang kita mau panggil
-			ResultSet rs = stmt.executeQuery(query);
-			while(rs.next()) {
-				System.out.println("Name: " + rs.getString("name"));
-				System.out.println("Price: " + rs.getInt("price"));
+		Scanner sc = new Scanner(System.in);
+
+		int user = 0, cabang = 0;
+		boolean call = true;
+
+		do{
+			do{
+				System.out.println("LaperAh");
+				System.out.println("1. Employee");
+				System.out.println("2. Customer");
+				System.out.println("3. Edit Employee");
+				System.out.println("4. Edit Customer");
+				System.out.print("Choose: ");
+				try {
+					user = sc.nextInt();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				sc.nextLine();
+			}while(user < 1 || user > 4);
+
+			do{
+				System.out.println("Pilih Cabang");
+				System.out.println("1. Surabaya");
+				System.out.println("2. Bandung");
+				System.out.println("3. Jakarta");
+				System.out.println("4. Samarinda");
+				System.out.println("5. Padang");
+				System.out.println("6. Bali");
+				System.out.println("7. Back");
+				System.out.print("Choose: ");
+				try {
+					cabang = sc.nextInt();
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				sc.nextLine();
+			}while(cabang < 1 || cabang > 7);
+			if(cabang == 7) continue;
+
+			if(user == 1){
+				System.out.println("hello");
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			else if(user == 2){
+				System.out.println("test");
+			}
+			else if(user == 3){
+				System.out.println("tes");
+			}
+			else if(user == 4){
+				System.out.println("helo");
+			}
+		}while(call);
 	}
 	
-	public static void insertData(Connection con, String query, String name, int price, int id) {
-		try {
-			PreparedStatement pst = con.prepareStatement(query);
-			pst.setString(1, name);
-			pst.setInt(2, price);
-			pst.setInt(3, id);
-			int check = pst.executeUpdate(); // value klo 0 berarti ga ada yang nambah
-			if(check != 0) System.out.println("Data inserted");
-			else System.out.println("Data not inserted");
-			getData(con,"select * from items");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void updateData(Connection con, String query, String name, int price, int id) {
-		try {
-			PreparedStatement pst = con.prepareStatement(query);
-			pst.setString(1, name);
-			pst.setInt(2, price);
-			pst.setInt(3, id);
-			int check = pst.executeUpdate(); // value klo 0 berarti ga ada yang ke update
-			if(check != 0) System.out.println("Data updated");
-			else System.out.println("Data not udpated");
-			getData(con,"select * from items");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void deleteData(Connection con, String query, String name) {
-		try {
-			PreparedStatement pst = con.prepareStatement(query);
-			pst.setString(1, name);
-			int check = pst.executeUpdate(); // value klo 0 berarti ga ada yang ke delete
-			if(check != 0) System.out.println("Data deleted");
-			else System.out.println("Data not deleted");
-			getData(con,"select * from items");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 }
